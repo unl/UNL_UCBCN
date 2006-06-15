3ca21c4ffeb6f87462ee6bfb5a33c7307736e0ff
Brett Bieber <brett.bieber@gmail.com>
2006-06-15 15:49:38 +0000

    EventListing class which holds an array of UNL_UCBCN_Events   this is a general purpose class that is used in both frontends for lists of events.

---
+++ UCBCN/EventListing.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This class holds all the events for the list.
+ * 
+ */
+class UNL_UCBCN_EventListing
+{
+	// Array of UNL_UCBCN_Event objects for this listing.
+	var $status;
+	var $events = array();
+	
+	function __construct()
+	{
+		
+	}
+}
