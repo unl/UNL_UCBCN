1cb83b59106bdb3b4d04a7255eae87a8305052c4
Brett Bieber <brett.bieber@gmail.com>
2006-08-29 21:51:49 +0000

    add calendar_id to user table.

--- UCBCN/User.php
+++ UCBCN/User.php
@@ -12,6 +12,7 @@ class UNL_UCBCN_User extends DB_DataObject
     public $__table = 'user';                            // table name
     public $uid;                             // string(100)  not_null primary_key
     public $account_id;                      // int(10)  not_null unsigned
+    public $calendar_id;                     // int(10)  unsigned
     public $accountstatus;                   // string(100)  
     public $datecreated;                     // datetime(19)  binary
     public $uidcreated;                      // string(100)  
