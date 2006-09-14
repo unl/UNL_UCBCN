28f05a7e580ad95518fa7cef53364b52beccce7c
Brett Bieber <brett.bieber@gmail.com>
2006-09-14 18:56:39 +0000

    New options for the constructor... the object will populate the events member variable with all the events for a given day. Pass parameters as instructed in the phpdoc block.

--- UCBCN/EventListing.php
+++ UCBCN/EventListing.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'UNL/UCBCN.php';
 
 /**
  * This class holds all the events for the list.
@@ -10,8 +11,63 @@ class UNL_UCBCN_EventListing
 	// Array of UNL_UCBCN_Event or UNL_UCBCN_EventInstance objects for this listing.
 	var $events = array();
 	
-	function __construct()
+	/**
+	 * Constructor
+	 *
+	 * @param string $type Optional parameter to fetch an event listing for types of events.
+	 * @param array $options options for the specific constructor to initialize the object.
+	 */
+	function __construct($type=NULL,$options=array())
 	{
-		
+	    
+		switch($type) {
+		    case 'day':
+		        $this->constructDayEventInstanceList($options);
+	        break;
+		    default:
+		    break;
+		}
 	}
+
+	/**
+	 * Populates events with a listing of events for the calendar given.
+	 *
+	 * @param array $options Associative array of options
+	 * 		'year'		int		Year of the events
+	 * 		'month'		int		Month
+	 * 		'day'		int		Day
+	 * 		'calendar'	UNL_UCBCN_Calendar	Calendar to fetch events for (optional).
+	 * 		'orderby'	string	ORDER BY sql clause.
+	 * 		
+	 */
+	function constructDayEventInstanceList($options)
+	{
+	    require_once 'Calendar/Day.php';
+		$day = new Calendar_Day($options['year'],$options['month'],$options['day']);
+		$eventdatetime = UNL_UCBCN::factory('eventdatetime');
+		if (isset($options['orderby'])) {
+		    $orderby = 	$options['orderby'];
+		} else {
+		    $orderby = 'eventdatetime.starttime ASC';
+		}
+		if (isset($options['calendar'])) {
+		    $calendar =& $options['calendar'];
+			$eventdatetime->query('SELECT DISTINCT eventdatetime.* FROM event,calendar_has_event,eventdatetime ' .
+							'WHERE calendar_has_event.calendar_id='.$calendar->id.' ' .
+									'AND (calendar_has_event.status =\'posted\' OR calendar_has_event.status =\'archived\') '.
+									'AND calendar_has_event.event_id = eventdatetime.event_id ' .
+									'AND eventdatetime.starttime LIKE \''.date('Y-m-d',$day->getTimestamp()).'%\' ' .
+							'ORDER BY '.$orderby);
+		} else {
+		    $calendar = NULL;
+			$eventdatetime->whereAdd('starttime LIKE \''.date('Y-m-d',$day->getTimestamp()).'%\'');
+			$eventdatetime->orderBy($orderby);
+			$eventdatetime->find();
+		}
+		while ($eventdatetime->fetch()) {
+			// Populate the events to display.
+			$this->events[] = new UNL_UCBCN_EventInstance($eventdatetime,$calendar);
+		}
+	}
+	
 }
