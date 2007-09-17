c0079c13ff1ba98b5a046ee7a1939a274443cd0c
Brett Bieber <brett.bieber@gmail.com>
2007-09-17 15:30:45 +0000

    Formmating, documentation and coding standards fixes.

--- UCBCN/Event_has_eventtype.php
+++ UCBCN/Event_has_eventtype.php
@@ -2,16 +2,30 @@
 /**
  * Table Definition for event_has_eventtype
  * 
- * @package    UNL_UCBCN
+ * PHP version 5
+ * 
+ * @category  Events 
+ * @package   UNL_UCBCN
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2007 Regents of the University of Nebraska
+ * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
+ * @link      http://pear.unl.edu/
  */
 
 /**
  * Require DB_DataObject to extend from it.
  */
 require_once 'DB/DataObject.php';
+
 /**
  * ORM for a record within the database.
- * @package UNL_UCBCN
+ * 
+ * @category  Events
+ * @package   UNL_UCBCN
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2007 Regents of the University of Nebraska
+ * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
+ * @link      http://pear.unl.edu/
  */
 class UNL_UCBCN_Event_has_eventtype extends DB_DataObject 
 {
@@ -19,8 +33,8 @@ class UNL_UCBCN_Event_has_eventtype extends DB_DataObject
     /* the code below is auto generated do not remove the above tag */
 
     public $__table = 'event_has_eventtype';             // table name
-    public $event_id;                        // int(10)  not_null unsigned
-    public $eventtype_id;                    // int(10)  not_null unsigned
+    public $event_id;                        // int(10)  not_null multiple_key unsigned
+    public $eventtype_id;                    // int(10)  not_null multiple_key unsigned
     public $id;                              // int(10)  not_null primary_key unsigned auto_increment
 
     /* Static get */
@@ -29,24 +43,39 @@ class UNL_UCBCN_Event_has_eventtype extends DB_DataObject
     /* the code above is auto generated do not remove the tag below */
     ###END_AUTOCODE
     
-    var $fb_hiddenFields			= array('event_id');
-    var $fb_excludeFromAutoRules	= array('event_id');
-    var $fb_fieldLabels			= array('eventtype_id'=>'Event Type');
-    var $fb_addFormHeader			= false;
-    var $fb_formHeaderText			= 'Event Type';
+    public $fb_hiddenFields         = array('event_id');
+    public $fb_excludeFromAutoRules = array('event_id');
+    public $fb_fieldLabels          = array('eventtype_id'=>'Event Type');
+    public $fb_addFormHeader        = false;
+    public $fb_formHeaderText       = 'Event Type';
     
-    function preGenerateForm(&$fb)
+    /**
+     * Called before the form is generated.
+     * 
+     * @param object &$fb Formbuilder object.
+     * 
+     * @return void
+     */
+    public function preGenerateForm(&$fb)
     {
-    	foreach ($this->fb_hiddenFields as $el) {
-    		$this->fb_preDefElements[$el] = HTML_QuickForm::createElement('hidden',$fb->elementNamePrefix.$el.$fb->elementNamePostfix);
-    	}
+        foreach ($this->fb_hiddenFields as $el) {
+            $this->fb_preDefElements[$el] = HTML_QuickForm::createElement('hidden', $fb->elementNamePrefix.$el.$fb->elementNamePostfix);
+        }
     }
     
-    function preProcessForm(&$values, &$formBuilder)
+    /**
+     * Called before the form is processed to modify values submitted.
+     * 
+     * @param array  &$values      Associative array of values submitted.
+     * @param object &$formBuilder Formbuilder object.
+     * 
+     * @return void
+     */
+    public function preProcessForm(&$values, &$formBuilder)
     {
-    	// Capture event_id foreign key if needed.
-    	if (isset($GLOBALS['event_id'])) {
-    		$values['event_id'] = $GLOBALS['event_id'];
-    	}
+        // Capture event_id foreign key if needed.
+        if (isset($GLOBALS['event_id'])) {
+            $values['event_id'] = $GLOBALS['event_id'];
+        }
     }
 }
