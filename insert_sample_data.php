9407088c09be49f514fd1fe1d90742f4e8cb3491
Brett Bieber <brett.bieber@gmail.com>
2008-06-19 20:46:25 +0000

    Remove UNL specific stuff... make this an event type insert script.

--- insert_sample_data.php
+++ insert_sample_data.php
@@ -10,33 +10,12 @@
  */
 require_once 'UNL/UCBCN.php';
 
-/**
- * require the UNL building class to pull building names to populate the locations table with data.
- */
-require_once 'UNL/Common/Building.php';
-
 error_reporting(E_ALL);
 
 $backend = new UNL_UCBCN(array('dsn'=>'mysql://eventcal:eventcal@localhost/eventcal'));
 
 echo '<pre>';
 DB_DataObject::debugLevel(1);
-
-/** Populate some locations in the database */
-$bldgs = new UNL_Common_Building();
-foreach ($bldgs->codes as $code=>$name) {
-	$location =& $backend->factory('location');
-	$location->name = $name;
-	if (!$location->find()) {
-		$location->city		= 'Lincoln';
-		$location->state	= 'NE';
-		$location->name		= $name;
-		$location->additionalpublicinfo = $code;
-		$location->insert();
-	} else {
-		echo "Sorry, $name already exists.\n";
-	}
-}
 
 /** Add some event types to the database */
 $eventtype = $backend->factory('eventtype');
@@ -103,35 +82,6 @@ foreach ($types as $type) {
 		$eventtype->name = $type;
 		$eventtype->description = $type;
 		$eventtype->insert();
-	}
-}
-
-$permissions = array(
-					'Event Create',
-					'Event Delete',
-					'Event Post',
-					'Event Send Event to Pending Queue',
-					'Event Edit',
-					'Event Recommend',
-					'Event Remove from Pending',
-					'Event Remove from Posted',
-					'Event Remove from System ',
-					'Event View Queue',
-					'Event Export',
-					'Event Upload',
-					'Calendar Add User',
-					'Calendar Format',
-					'Calendar Change Format',
-					'Calendar Delete');
-foreach ($permissions as $p_type) {
-	$p = $backend->factory('permission');
-	$p->name = $p_type;
-	if (!$p->find()) {
-		$p->name = $p_type;
-		$p->description = $p_type;
-		$p->insert();
-	} else {
-		echo "Sorry, $p_type already exists.\n";
 	}
 }
 
