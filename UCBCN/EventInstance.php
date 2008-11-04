eaa9b2c9cb96c3f280e9539d2b3aa4948f10b7fb
Brett Bieber <brett.bieber@gmail.com>
2008-11-04 15:33:00 +0000

    coding standards fixes

--- UCBCN/EventInstance.php
+++ UCBCN/EventInstance.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * This class is a simple container object for all the details related to an event and its details.
+ * This class is a simple container object for all the details related to an event
+ * and its details.
  * 
  * PHP version 5
  * 
@@ -16,10 +17,12 @@
  * Requires the UNL_UCBCN backend class.
  */
 require_once 'UNL/UCBCN.php';
+
 /**
  * Generic object to hold a single event, and a single event date time.
  * Together, the two represent an instance of an event at a location.
  * 
+ * @category  Events
  * @package   UNL_UCBCN
  * @author    Brett Bieber <brett.bieber@gmail.com>
  * @copyright 2008 Regents of the University of Nebraska
@@ -28,64 +31,65 @@ require_once 'UNL/UCBCN.php';
  */
 class UNL_UCBCN_EventInstance extends UNL_UCBCN
 {
-	/** UNL_UCBCN_Event object */
-	var $event;
-	/** UNL_UCBCN_Eventdatetime object */
-	var $eventdatetime;
-	/** Optional calendar associated with this event instance */
-	var $calendar;	
-	/** URL to this event instance */
-	var $url;
-	
-	/**
-	 * constructor
-	 * 
-	 * @param mixed int|UNL_UCBCN_Eventdatetime The id for this record in the database, or the actual object.
-	 * @param mixed int|UNL_UCBCN_Calendar... optional parameter for the calendar this event is for.
-	 */
-	function __construct($edt,$calendar=NULL)
-	{
-		if (is_object($edt) && get_class($edt)=='UNL_UCBCN_Eventdatetime') {
-			$this->eventdatetime = clone($edt);
-			$this->event = $this->eventdatetime->getLink('event_id');
-		} else {
-			$this->eventdatetime = UNL_UCBCN::factory('eventdatetime');
-			if ($this->eventdatetime->get($edt)) {
-				$this->event = $this->eventdatetime->getLink('event_id');
-			} else {
-				return new UNL_UCBCN_Error('Could not find that event instance.');
-			}
-		}
-		if (isset($calendar)) {
-			if (is_object($calendar) && get_class($calendar)=='UNL_UCBCN_Calendar') {
-				$this->calendar = clone($calendar);
-			} else {
-				$c = UNL_UCBCN::factory('calendar');
-				if ($c->get($calendar)) {
-					$this->calendar = $c;
-				}
-			}
-		}
-		$this->url = $this->getURL();
-	}
-	
-	/**
-	 * This function returns the URL for this event instance.
-	 * @return string URL to this event instance.
-	 */
-	function getURL()
-	{
-		$date = explode('-',$this->eventdatetime->starttime);
-		$day = explode(' ',$date[2]);
-		$f = array(	'y'=>$date[0],
-					'm'=>$date[1],
-					'd'=>$day[0],
-					'eventdatetime_id'=>$this->eventdatetime->id);
-		if (isset($this->calendar)) {
-			$f['calendar'] = $this->calendar->id;
-		}
-		return UNL_UCBCN_Frontend::formatURL($f);
-	}
+    /** UNL_UCBCN_Event object */
+    public $event;
+    /** UNL_UCBCN_Eventdatetime object */
+    public $eventdatetime;
+    /** Optional calendar associated with this event instance */
+    public $calendar;    
+    /** URL to this event instance */
+    public $url;
+    
+    /**
+     * constructor
+     * 
+     * @param int|UNL_UCBCN_Eventdatetime $edt      eventdatetime.id in the database, or the actual object.
+     * @param int|UNL_UCBCN_Calendar      $calendar optional parameter for the calendar this event is for.
+     */
+    function __construct($edt, $calendar=null)
+    {
+        if (is_object($edt) && get_class($edt)=='UNL_UCBCN_Eventdatetime') {
+            $this->eventdatetime = clone($edt);
+            $this->event         = $this->eventdatetime->getLink('event_id');
+        } else {
+            $this->eventdatetime = UNL_UCBCN::factory('eventdatetime');
+            if ($this->eventdatetime->get($edt)) {
+                $this->event = $this->eventdatetime->getLink('event_id');
+            } else {
+                return new UNL_UCBCN_Error('Could not find that event instance.');
+            }
+        }
+        if (isset($calendar)) {
+            if (is_object($calendar) && get_class($calendar)=='UNL_UCBCN_Calendar') {
+                $this->calendar = clone($calendar);
+            } else {
+                $c = UNL_UCBCN::factory('calendar');
+                if ($c->get($calendar)) {
+                    $this->calendar = $c;
+                }
+            }
+        }
+        $this->url = $this->getURL();
+    }
+    
+    /**
+     * This function returns the URL for this event instance.
+     * 
+     * @return string URL to this event instance.
+     */
+    function getURL()
+    {
+        $date = explode('-', $this->eventdatetime->starttime);
+        $day  = explode(' ', $date[2]);
+        $f    = array('y'=>$date[0],
+                      'm'=>$date[1],
+                      'd'=>$day[0],
+                      'eventdatetime_id'=>$this->eventdatetime->id);
+        if (isset($this->calendar)) {
+            $f['calendar'] = $this->calendar->id;
+        }
+        return UNL_UCBCN_Frontend::formatURL($f);
+    }
 }
 
 ?>
