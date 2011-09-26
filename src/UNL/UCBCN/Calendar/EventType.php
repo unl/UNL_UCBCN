4dbb2582724ca8902645f87459f9b501c9682156
Michael Fairchild <mfairchild365@gmail.com>
2011-09-26 15:41:20 -0500

    capitalize Type

--- src/UNL/UCBCN/Calendar/EventType.php
+++ src/UNL/UCBCN/Calendar/EventType.php
@@ -22,7 +22,7 @@
  * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
  * @link      http://code.google.com/p/unl-event-publisher/
  */
-class UNL_UCBCN_Calendar_Eventtype extends UNL_UCBCN_Record
+class UNL_UCBCN_Calendar_EventType extends UNL_UCBCN_Record
 {
 
     public $id;                              // int(10)  not_null primary_key unsigned auto_increment
