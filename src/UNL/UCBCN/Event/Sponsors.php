b466e325906268c4fddd6ad999d015cc95bcf055
Michael Fairchild <mfairchild365@gmail.com>
2011-09-26 14:13:27 -0500

    Remove underscores from class names

---
+++ src/UNL/UCBCN/Event/Sponsors.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Table Definition for event_has_sponsor
+ *
+ * PHP version 5
+ *
+ * @category  Events
+ * @package   UNL_UCBCN
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2009 Regents of the University of Nebraska
+ * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
+ * @link      http://code.google.com/p/unl-event-publisher/
+ */
+
+/**
+ * ORM for a record within the database.
+ *
+ * @package   UNL_UCBCN
+ * @author    Brett Bieber <brett.bieber@gmail.com>
+ * @copyright 2009 Regents of the University of Nebraska
+ * @license   http://www1.unl.edu/wdn/wiki/Software_License BSD License
+ * @link      http://code.google.com/p/unl-event-publisher/
+ */
+class UNL_UCBCN_Event_Sponsors extends UNL_UCBCN_Record
+{
+
+    public $id;                              // int(10)  not_null primary_key unsigned auto_increment
+    public $event_id;                        // int(10)  not_null multiple_key unsigned
+    public $sponsor_id;                      // int(10)  not_null multiple_key unsigned
+
+    public function getTable()
+    {
+        return 'event_has_sponsor';
+    }
+
+    function table()
+    {
+        return array(
+            'id'=>129,
+            'event_id'=>129,
+            'sponsor_id'=>129,
+        );
+    }
+
+    function keys()
+    {
+        return array(
+            'id',
+        );
+    }
+    
+    function sequenceKey()
+    {
+        return array('id',true);
+    }
+    
+    function links()
+    {
+        return array('event_id'   => 'event:id',
+                     'sponsor_id' => 'sponsor:id');
+    }
+
+}
