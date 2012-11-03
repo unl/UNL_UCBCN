72e0eb3f294369028699e735202672da05a63461
Brett Bieber <brett.bieber@gmail.com>
2012-11-02 23:21:03 -0500

    Add a model for a calendar event occurrence (formerly called event instance)

---
+++ src/UNL/UCBCN/Calendar/Event/Occurrence.php
@@ -0,0 +1,10 @@
+<?php
+namespace UNL\UCBCN\Calendar\Event;
+
+class Occurrence extends \UNL\UCBCN\Event\Occurrence
+{
+    function __construct($options = array())
+    {
+        // Assuming we must have calendar_id and event_id here
+    }
+}
